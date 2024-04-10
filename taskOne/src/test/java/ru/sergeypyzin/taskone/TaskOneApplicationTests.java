69a8322db5140f54f20443e57f4aeeae9a34994d
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-04-10 10:06:07 +0300

    commit init

---
+++ taskOne/src/test/java/ru/sergeypyzin/taskone/TaskOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergeypyzin.taskone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
