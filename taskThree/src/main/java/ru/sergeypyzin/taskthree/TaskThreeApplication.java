96dc5334c7e44cc6639f58f625e6d50f662059ca
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-04-10 16:51:25 +0300

    start taskThree

---
+++ taskThree/src/main/java/ru/sergeypyzin/taskthree/TaskThreeApplication.java
@@ -0,0 +1,13 @@
+package ru.sergeypyzin.taskthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskThreeApplication.class, args);
+	}
+
+}
---
+++ taskThree/src/test/java/ru/sergeypyzin/taskthree/TaskThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergeypyzin.taskthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
